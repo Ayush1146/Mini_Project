43b77bdc8b0202d8a6f48833f81836cca1cfe8ff
Ayush Sharma <sharmayush009@gmail.com>
2021-04-15 00:36:03 +0530

    added ' ' in char input in test cases

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -32,42 +32,42 @@ int main()
 
 /* Write all the test functions */ 
 void test_getcode(void) {
-TEST_ASSERT_EQUAL(0, getcode(B)); 
-TEST_ASSERT_EQUAL(1, getcode(N)); 
-TEST_ASSERT_EQUAL(2, getcode(R)); 
-TEST_ASSERT_EQUAL(3, getcode(O)); 
-TEST_ASSERT_EQUAL(4, getcode(Y));
-TEST_ASSERT_EQUAL(5, getcode(G)); 
-TEST_ASSERT_EQUAL(6, getcode(E)); 
-TEST_ASSERT_EQUAL(7, getcode(V));
-TEST_ASSERT_EQUAL(8, getcode(A));
-TEST_ASSERT_EQUAL(9, getcode(W));
+TEST_ASSERT_EQUAL(0, getcode('B')); 
+TEST_ASSERT_EQUAL(1, getcode('N')); 
+TEST_ASSERT_EQUAL(2, getcode('R')); 
+TEST_ASSERT_EQUAL(3, getcode('O')); 
+TEST_ASSERT_EQUAL(4, getcode('Y'));
+TEST_ASSERT_EQUAL(5, getcode('G')); 
+TEST_ASSERT_EQUAL(6, getcode('E')); 
+TEST_ASSERT_EQUAL(7, getcode('V'));
+TEST_ASSERT_EQUAL(8, getcode('A'));
+TEST_ASSERT_EQUAL(9, getcode('W'));
  
 }
 
 void test_getmul(void) {
-TEST_ASSERT_EQUAL(1.0, getcode(B)); 
-TEST_ASSERT_EQUAL(10.0, getcode(N)); 
-TEST_ASSERT_EQUAL(100.0, getcode(R)); 
-TEST_ASSERT_EQUAL(1000.0, getcode(O)); 
-TEST_ASSERT_EQUAL(10000.0, getcode(Y));
-TEST_ASSERT_EQUAL(100000.0, getcode(G)); 
-TEST_ASSERT_EQUAL(1000000.0, getcode(E)); 
-TEST_ASSERT_EQUAL(10000000.0, getcode(V));
-TEST_ASSERT_EQUAL(0.1, getcode(C));
-TEST_ASSERT_EQUAL(0.01, getcode(S));  
+TEST_ASSERT_EQUAL(1.0, getcode('B')); 
+TEST_ASSERT_EQUAL(10.0, getcode('N')); 
+TEST_ASSERT_EQUAL(100.0, getcode('R')); 
+TEST_ASSERT_EQUAL(1000.0, getcode('O')); 
+TEST_ASSERT_EQUAL(10000.0, getcode('Y'));
+TEST_ASSERT_EQUAL(100000.0, getcode('G')); 
+TEST_ASSERT_EQUAL(1000000.0, getcode('E')); 
+TEST_ASSERT_EQUAL(10000000.0, getcode('V'));
+TEST_ASSERT_EQUAL(0.1, getcode('C'));
+TEST_ASSERT_EQUAL(0.01, getcode('S'));  
 }
 
 void test_gettol(void) {
 
-TEST_ASSERT_EQUAL(1.0, getcode(N)); 
-TEST_ASSERT_EQUAL(2.0, getcode(R)); 
-TEST_ASSERT_EQUAL(0.5, getcode(G)); 
-TEST_ASSERT_EQUAL(0.25, getcode(E)); 
-TEST_ASSERT_EQUAL(0.1, getcode(V));
-TEST_ASSERT_EQUAL(0.05, getcode(A));
-TEST_ASSERT_EQUAL(5.0, getcode(C));
-TEST_ASSERT_EQUAL(10.0, getcode(S));
-TEST_ASSERT_EQUAL(20.0, getcode(X));
+TEST_ASSERT_EQUAL(1.0, getcode('N')); 
+TEST_ASSERT_EQUAL(2.0, getcode('R')); 
+TEST_ASSERT_EQUAL(0.5, getcode('G')); 
+TEST_ASSERT_EQUAL(0.25, getcode('E')); 
+TEST_ASSERT_EQUAL(0.1, getcode('V'));
+TEST_ASSERT_EQUAL(0.05, getcode('A'));
+TEST_ASSERT_EQUAL(5.0, getcode('C'));
+TEST_ASSERT_EQUAL(10.0, getcode('S'));
+TEST_ASSERT_EQUAL(20.0, getcode('X'));
 }
 
