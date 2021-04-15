5e023f64b022d5f12e464c1307f933ef068279d9
Ayush Sharma <sharmayush009@gmail.com>
2021-04-15 17:03:14 +0530

    documentation

--- 3_Implementation/inc/documentation_main.md
+++ 3_Implementation/inc/documentation_main.md
@@ -1,2 +1,2 @@
-@mainpage Calculator Application by Bharath G
-@subpage calculator_operations.h
+@mainpage Resistor Application by Ayush Sharma
+@subpage operations.h
--- 3_Implementation/inc/operations.h
+++ 3_Implementation/inc/operations.h
@@ -1,15 +1,41 @@
+/**
+ * @file operations.h
+ * @author Ayush Sharma(sharmayush009@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __OPERATIONS_H__
 #define __OPERATIONS_H__
 
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * @function to get code for the particular colour of resistor band.
+ * 
+ * @param c : input from the user i.e. the Letter representing the colour band
+ * @return int : Output code value of the colour band
+ */
 int getcode(char c);
 
-
+/**
+ * @function to get multiplier for the particular colour of resistor band.
+ * 
+ * @param c : input from the user i.e. the Letter representing the colour band
+ * @return float : Output multiplier value of the colour band
+ */
 float getmul(char c);
 
+/**
+ * @function to get tolerance for the particular colour of resistor band.
+ * 
+ * @param c : input from the user i.e. the Letter representing the colour band
+ * @return float : Output tolerance value of the colour band
+ */
 
 float gettol(char c);
 
